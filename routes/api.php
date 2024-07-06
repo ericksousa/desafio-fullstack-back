0af688a384e715cca20cd5794080449b55476c84
Erick Sousa <23417131+ericksousa@users.noreply.github.com>
2024-07-05 22:53:23 -0300

    feat(auth): cria api para registro/login de usuário

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Auth\AuthController;
+
+Route::controller(AuthController::class)->group(function() {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
