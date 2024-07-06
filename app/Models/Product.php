040c751e31458d72aae754804f3cc327ff61c4de
Erick Sousa <23417131+ericksousa@users.noreply.github.com>
2024-07-06 11:59:27 -0300

    feat(produto): cria observer para limpeza de cache

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Observers\ProductObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+#[ObservedBy([ProductObserver::class])]
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
